e919582d42cd15fcabbf97c061daea8c04a45d76
Michał Ordon <AeroRD.Craft@gmail.com>
2023-05-13 12:38:13 +0200

    Added possibility of addition, substraction, multiplication, integer division, modulo, logical NOT, greater than. Minor typos have been corrected

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 def interpret(code):
-    # output = ''
+    output = ''
     stack = []
     moving_char = ''  # going this way till direction is changed
     moving_char_table = ['<', '>', '^', 'v']  # possibilities of move
-    avaliable_digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    available_digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    available_math_signs = ['+', '-', '*', '/', '%', '`']
 
     code = code.split('\n')
     row, position = 0, 0
@@ -19,6 +20,26 @@ def interpret(code):
         if moving_char == 'v':
             return row_num + 1, pos_num
 
+    def make_a_math(a, b, math_sign):
+        if math_sign == '+':
+            return str(a + b)
+        if math_sign == '-':
+            return str(b - a)
+        if math_sign == '*':
+            return str(a * b)
+        if math_sign == '/':  # rounded down
+            if a == 0:
+                return '0'
+            return str(int(b / a))
+        if math_sign == '%':
+            if a == 0:
+                return '0'
+            return str(b % a)
+        if math_sign == '`':
+            if b > a:
+                return '1'
+            return '0'
+
     while current_char != '@':
         if code[row][position] == ' ':  # if new char is empty, stay with old one
             continue
@@ -27,12 +48,24 @@ def interpret(code):
             moving_char = current_char
         row, position = make_a_move(row, position)
 
-        if current_char in avaliable_digits:
+        if current_char in available_digits:
             stack.append(int(current_char))
             continue
+        if current_char in available_math_signs:
+            num1 = stack.pop()
+            num2 = stack.pop()
+            output += make_a_math(num1, num2, current_char)
+            continue
+        if current_char == '!':
+            num1 = stack.pop()
+            if num1 == 0:
+                output += '1'
+            else:
+                output += '0'
+            continue
 
-    return stack
+    return output
 
 
-print(interpret('>112@'))
+# print(interpret('>132-@'))
 # print(interpret('>987v>.v\nv456<  :\n>321 ^ _@'))
