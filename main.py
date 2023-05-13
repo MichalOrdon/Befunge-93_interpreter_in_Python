823e319b8c94e25808f21a21ad2becdf539f25e1
Michał Ordon <AeroRD.Craft@gmail.com>
2023-05-13 11:05:48 +0200

    now it starts properly and accepts digits to the stack

--- main.py
+++ main.py
@@ -1,38 +1,38 @@
 def interpret(code):
-    output = ''
+    # output = ''
     stack = []
-    moving_char = ''
-    moving_char_table = []
+    moving_char = ''  # going this way till direction is changed
+    moving_char_table = ['<', '>', '^', 'v']  # possibilities of move
+    avaliable_digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
     code = code.split('\n')
     row, position = 0, 0
     current_char = code[row][position]
 
-    def make_a_move(row, position):
+    def make_a_move(row_num, pos_num):
         if moving_char == '>':
-            return row, position + 1
+            return row_num, pos_num + 1
         if moving_char == '<':
-            return row, position - 1
+            return row_num, pos_num - 1
         if moving_char == '^':
-            return row - 1, position
+            return row_num - 1, pos_num
         if moving_char == 'v':
-            return row + 1, position
+            return row_num + 1, pos_num
 
     while current_char != '@':
-        make_a_move(row, position)
         if code[row][position] == ' ':  # if new char is empty, stay with old one
             continue
-        else:
-            current_char = code[row][position]
-        if type(current_char) == int:
-            stack.insert(-1, current_char)
-            continue
+        current_char = code[row][position]
         if current_char in moving_char_table:
             moving_char = current_char
+        row, position = make_a_move(row, position)
+
+        if current_char in avaliable_digits:
+            stack.append(int(current_char))
             continue
 
     return stack
 
 
-print(interpret('1,1,2,@'))
+print(interpret('>112@'))
 # print(interpret('>987v>.v\nv456<  :\n>321 ^ _@'))
